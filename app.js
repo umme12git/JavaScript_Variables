f69cada7a979344803eed351fcb11140987de58e
umme <unnipa01@yahoo.com>
2021-09-08 00:23:16 -0500

    initial copy

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
+const firstName = "Umme";
+let lastName = "Nipa";
+var age = 52;
+
+
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+let city = "Chicago";
+let passtime = "watching TV";
+let myStory = `\Hello my name is ${fullName}.  I live in ${city}.  I like ${passtime}.`;
+console.log(myStory);
+
+
 // Exercise 1
 
 // Exercise 2
